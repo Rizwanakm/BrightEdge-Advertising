4880d3566c7acbe85ec1482ea1f64069e14d289b
Rizwanakm <you@example.com>
2025-03-08 16:48:16 +0530

    changes

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevents page refresh
+    alert("Thank you for contacting us! We will get back to you soon.");
+});
